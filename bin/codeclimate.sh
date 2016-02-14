97a8f588eed67bf2c608816ce34ccbf2dd55f17b
Josué Piña <jabdhel@gmail.com>
2016-02-14 22:23:49 +0100

    Remove unused code

--- bin/codeclimate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-$(dirname $0)/../vendor/bin/test-reporter --stdout > codeclimate.json
-curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.2.0)' https://codeclimate.com/test_reports
